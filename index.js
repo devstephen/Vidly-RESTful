8dfa9bdde7383666a698a476118a7c04738cfd0b
Fatolu Stephen <smjssp@gmail.com>
2023-11-09 16:10:38 +0100

    Add customers module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const genres = require('./routes/genres')
+const customers = require('./routes/customers')
 const express = require('express')
 const app = express()
 
@@ -10,6 +11,7 @@ mongoose
 
 app.use(express.json())
 app.use('/api/genres', genres)
+app.use('/api/customers', customers)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
--- routes/genres.js
+++ routes/genres.js
@@ -74,7 +74,7 @@ router.get('/:id', async (req, res) => {
 
 function validateGenre(genre) {
   const schema = {
-    name: Joi.string().min(3).required(),
+    name: Joi.string().min(5).max(50).required(),
   }
 
   return Joi.validate(genre, schema)
