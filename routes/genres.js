92bdf5ac1042e5234801bbb3e039ba82d6194541
Fatolu Stephen <smjssp@gmail.com>
2023-11-11 15:54:01 +0100

    Implement admin auth

--- routes/genres.js
+++ routes/genres.js
@@ -2,6 +2,7 @@ const auth = require('../middleware/auth')
 const { Genre, validate } = require('../models/genre')
 const mongoose = require('mongoose')
 const express = require('express')
+const admin = require('../middleware/admin')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
@@ -37,7 +38,7 @@ router.put('/:id', async (req, res) => {
   res.send(genre)
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id)
 
   if (!genre)
