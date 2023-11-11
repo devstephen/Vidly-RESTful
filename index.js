8bc8005d25211bf4ffa3d16179c751c36a63bcc1
Fatolu Stephen <smjssp@gmail.com>
2023-11-11 09:10:16 +0100

    Add user model and route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
+const users = require('./routes/users')
 const express = require('express')
 const app = express()
 
@@ -18,6 +19,7 @@ app.use('/api/genres', genres)
 app.use('/api/customers', customers)
 app.use('/api/movies', movies)
 app.use('/api/rentals', rentals)
+app.use('/api/users', users)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const { User, validate } = require('../models/users')
+const mongoose = require('mongoose')
+const express = require('express')
+const router = express.Router()
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body)
+
+  if (error) return res.status(400).send(error.details[0].message)
+
+  let user = await User.findOne({ email: req.body.email })
+
+  if (user) return res.status(400).send('User is already registered ')
+
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  })
+
+  await user.save()
+
+  res.send(user)
+})
+
+module.exports = router
