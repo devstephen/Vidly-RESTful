9c4a344a63ce06d315ef66e2a10854f74bdb6760
Fatolu Stephen <smjssp@gmail.com>
2023-11-11 11:23:19 +0100

    Implement jwt

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config')
 const Joi = require('joi')
 Joi.objectId = require('joi-objectid')(Joi)
 const mongoose = require('mongoose')
@@ -6,9 +7,14 @@ const customers = require('./routes/customers')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
 const users = require('./routes/users')
+const auth = require('./routes/auth')
 const express = require('express')
 const app = express()
 
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined')
+  process.exit(1)
+}
 mongoose
   .connect('mongodb://localhost/vidly')
   .then(() => console.log('Connected to MongoDB...'))
@@ -20,6 +26,7 @@ app.use('/api/customers', customers)
 app.use('/api/movies', movies)
 app.use('/api/rentals', rentals)
 app.use('/api/users', users)
+app.use('/api/auth', auth)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const _ = require('lodash')
 const { User, validate } = require('../models/users')
@@ -17,10 +19,11 @@ router.post('/', async (req, res) => {
   user = new User(_.pick(req.body, ['name', 'email', 'password']))
   const salt = await bcrypt.genSalt(10)
   user.password = await bcrypt.hash(user.password, salt)
-
   await user.save()
 
-  res.send(_.pick(user, ['_id', 'name', 'email']))
+  const token = user.generateAuthToken()
+
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
 })
 
 module.exports = router
